6e30f18d0fa94b8d8226b2803b2fb30ee2c455ae
kushal <kushaldeveloper@gmail.com>
2020-07-14 11:20:14 -0600

    add tests

--- src/app/logger.service.spec.ts
+++ src/app/logger.service.spec.ts
@@ -13,4 +13,19 @@ describe('LoggerService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should add one message to log', () => {
+    service.log(`test message`);
+    expect(service.logs.length === 1);
+    expect(service.logs.findIndex(message => message === `test message`) === 0).toBeTrue();
+  });
+
+
+  it('should add two messages to log', () => {
+    service.log(`test message`);
+    service.log(`second message`);
+    expect(service.logs.length === 2);
+    expect(service.logs.findIndex(message => message === `test message`) === 0).toBeTrue();
+    expect(service.logs.findIndex(message => message === `second message`) === 1).toBeTrue();
+  });
 });
