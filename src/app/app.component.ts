0cf548e44a06f6a6df2c1b2df11623737cb40b06
kushal <kushaldeveloper@gmail.com>
2020-07-14 12:25:44 -0600

    use getAbe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { GetAbeJsonService } from './get-abe-json.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ReactiveMaterial';
+
+  constructor(
+    private abeService: GetAbeJsonService
+  ) { }
+
+  ngOnInit(): void {
+    this.abeService.getAbe().subscribe((response) => {
+      console.log({ response });
+    })
+  }
 }
