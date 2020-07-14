42643ce43f7b77e3edba87d8a2c64f1df3e2806b
kushal <kushaldeveloper@gmail.com>
2020-07-14 14:21:04 -0600

    add github pages shell script

---
+++ githubpages.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+
+cd /home/kushal/src/html/myreactivematerialfirebase/ReactiveMaterial/;
+time ng update;
+time ng build --prod;
+time rsync -av /home/kushal/src/html/myreactivematerialfirebase/ReactiveMaterial/dist/ReactiveMaterial/ /home/kushal/src/html/myreactivematerialfirebase/reactivematerialgithubio/;
+cd /home/kushal/src/html/myreactivematerialfirebase/reactivematerialgithubio/;
+git add .;
+time git commit -m "add all files" -m "get ready to deploy";
+time git push origin master;
