6290f61f38449a4f592cc119ca302aba46029571
kushal <kushaldeveloper@gmail.com>
2020-07-15 16:26:29 -0600

    add ng test

--- githubpages.sh
+++ githubpages.sh
@@ -5,6 +5,7 @@ cd /home/kushal/src/html/myreactivematerialfirebase/ReactiveMaterial/;
 time ng update;
 time ng update @angular/core @angular/cli @angular/cdk @angular/material rxjs
 time ng build --prod;
+time ng test --watch=false;
 time rsync -av /home/kushal/src/html/myreactivematerialfirebase/ReactiveMaterial/dist/ReactiveMaterial/ /home/kushal/src/html/myreactivematerialfirebase/reactivematerialgithubio/;
 cd /home/kushal/src/html/myreactivematerialfirebase/reactivematerialgithubio/;
 git add .;
