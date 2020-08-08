40b9ddf1ad59ad714bc62d80a3f35a76854f0f1b
kushal <kushaldeveloper@gmail.com>
2020-08-08 16:03:56 -0600

    update angular
    The installed local Angular CLI version is older than the latest stable version.
    Installing a temporary version to perform the update.
    Installing packages for tooling via yarn.
    Installed packages for tooling via yarn.
    Using package manager: 'yarn'
    Collecting installed dependencies...
    Found 33 dependencies.
        We analyzed your package.json, there are some packages to update:
    
          Name                               Version                  Command to update
         --------------------------------------------------------------------------------
          @angular/cdk                       10.1.0 -> 10.1.2         ng update @angular/cdk
          @angular/cli                       10.0.4 -> 10.0.5         ng update @angular/cli
          @angular/core                      10.0.5 -> 10.0.8         ng update @angular/core
          @angular/material                  10.1.0 -> 10.1.2         ng update @angular/material
          rxjs                               6.6.0 -> 6.6.2           ng update rxjs
    
    real	0m17.432s
    user	0m12.960s
    sys	0m2.719s
    The installed local Angular CLI version is older than the latest stable version.
    Installing a temporary version to perform the update.
    Installing packages for tooling via yarn.
    Installed packages for tooling via yarn.
    Using package manager: 'yarn'
    Collecting installed dependencies...
    Found 33 dependencies.
    Fetching dependency metadata from registry...
        Updating package.json with dependency @angular/cli @ "10.0.5" (was "10.0.4")...
        Updating package.json with dependency @angular/cdk @ "10.1.2" (was "10.1.0")...
        Updating package.json with dependency @angular/core @ "10.0.8" (was "10.0.5")...
        Updating package.json with dependency @angular/material @ "10.1.2" (was "10.1.0")...
        Updating package.json with dependency rxjs @ "6.6.2" (was "6.6.0")...
        Updating package.json with dependency @angular-devkit/build-angular @ "0.1000.5" (was "0.1000.4")...
        Updating package.json with dependency @angular/compiler-cli @ "10.0.8" (was "10.0.5")...
        Updating package.json with dependency @angular/common @ "10.0.8" (was "10.0.5")...
        Updating package.json with dependency @angular/compiler @ "10.0.8" (was "10.0.5")...
        Updating package.json with dependency @angular/platform-browser-dynamic @ "10.0.8" (was "10.0.5")...
        Updating package.json with dependency @angular/animations @ "10.0.8" (was "10.0.5")...
        Updating package.json with dependency @angular/router @ "10.0.8" (was "10.0.5")...
        Updating package.json with dependency @angular/forms @ "10.0.8" (was "10.0.5")...
        Updating package.json with dependency @angular/platform-browser @ "10.0.8" (was "10.0.5")...
        Updating package.json with dependency @angular/service-worker @ "10.0.8" (was "10.0.5")...
    UPDATE package.json (1407 bytes)
    ✔ Packages installed successfully.
    
    real	0m43.662s
    user	0m29.690s
    sys	0m11.190s

--- githubpages.sh
+++ githubpages.sh
@@ -3,10 +3,14 @@
 
 cd /home/kushal/src/html/myreactivematerialfirebase/ReactiveMaterial/;
 time ng update;
-time ng update @angular/core @angular/cli @angular/cdk @angular/material rxjs
+time ng update @angular/core @angular/cli @angular/cdk @angular/material rxjs;
+time ng update;
+time git add .;
+time git commit -m "update angular" -m "angular {cli, cdk, material}, and rxjs";
 time ng build --prod;
 time ng test --watch=false;
 time rsync -av /home/kushal/src/html/myreactivematerialfirebase/ReactiveMaterial/dist/ReactiveMaterial/ /home/kushal/src/html/myreactivematerialfirebase/reactivematerialgithubio/;
+time git push origin --all;
 cd /home/kushal/src/html/myreactivematerialfirebase/reactivematerialgithubio/;
 git add .;
 time git commit -m "add all files" -m "get ready to deploy";
